51b8cf8b84028f325f2a35514440d9bd67520ba9
foroughafkhami <foroughfarnaz80@gmail.com>
2022-06-12 18:14:07 +0430

    Control the position of the robot in one direction

---
+++ robot1_HW1.py
@@ -0,0 +1,47 @@
+# rcj_soccer_player controller - ROBOT B1
+
+# Feel free to import built-in libraries
+import math  # noqa: F401
+
+# You can also import scripts that you put into the folder with controller
+import utils
+from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
+
+
+class MyRobot1(RCJSoccerRobot):
+    def run(self):
+        x_desired = -0.09
+        kp = 20
+        while self.robot.step(TIME_STEP) != -1:
+            if self.is_new_data():
+                data = self.get_new_data()  # noqa: F841
+
+                while self.is_new_team_data():
+                    team_data = self.get_new_team_data()  # noqa: F841
+                    # Do something with team data
+
+                if self.is_new_ball_data():
+                    ball_data = self.get_new_ball_data()
+                else:
+                    # If the robot does not see the ball, stop motors
+                    self.left_motor.setVelocity(0)
+                    self.right_motor.setVelocity(0)
+                    continue
+
+                # Get data from compass
+                heading = self.get_compass_heading()  # noqa: F841
+    
+                # Get GPS coordinates of the robot
+                robot_pos = self.get_gps_coordinates()  # noqa: F841
+                #print("heading",math.degrees(heading))
+                
+                # Get data from sonars
+                sonar_values = self.get_sonar_values()  # noqa: F841
+                
+                
+                
+                error_x = robot_pos[1] - x_desired
+                u_x = kp * error_x
+                
+                self.left_motor.setVelocity(u_x)
+                self.right_motor.setVelocity(u_x)
